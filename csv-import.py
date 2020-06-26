e07132b7c884e9ad334a1d5e7f961bf4960a5b21
Aurélie Ferron <ferrau10@gmail.com>
2020-06-26 14:26:30 +0200

    final

--- csv-import.py
+++ csv-import.py
@@ -18,18 +18,5 @@ def wrangle(input_object):
     requests.post("http://127.0.0.1:5000/reviewer" , json=input_dict)
 
 
-def wrangle2(input_object):
-    input_dict = input_object.to_dict()
-
-    reviewer_request = requests.get("http://127.0.0.1:5000/reviewer/{}".format(input_dict["reviewerName"]))
-    if reviewer_request.status_code == 400:
-        reviewer_request = requests.post("http://127.0.0.1:5000/reviewer" , json=input_dict)
-
-    input_dict["reviewer_id"] = reviewer_request.text
-    requests.post("http://127.0.0.1:5000/review" , json=input_dict)
-
-
-
 
 df.apply(wrangle, axis=1)
-df.apply(wrangle2, axis=1)
